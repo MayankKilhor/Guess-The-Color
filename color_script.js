3a435a92fbdcf144fcd346b6b5d113db5e24780c
Rory William <64885374+TheRoryWillAim@users.noreply.github.com>
2020-05-24 12:44:25 +0530

    Update color_script.js

--- color_script.js
+++ color_script.js
@@ -1,1 +1,15 @@
 
+var colors= [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+
+for(var i=0;i < squares.length;i++){
+    suares[i].style.backgroundColor = colors[i];
+}
